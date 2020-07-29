ebea3ac103d7cd7d25123e6c60c818e955178812
nossbigg <chenggibson@gmail.com>
2020-07-29 11:35:10 +0800

    Fetch unprotected info for SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { GetServerSideProps } from "next";
 
-const IndexPage: React.FC = () => {
-  return <div>Hello World!</div>;
+type IndexPageType = IndexPageServerSideProps;
+type IndexPageServerSideProps = { unprotectedInfo: Object };
+
+const IndexPage: React.FC<IndexPageType> = (props) => {
+  const { unprotectedInfo } = props;
+
+  return (
+    <div>
+      Hello World!
+      <br />
+      Unprotected Info: {JSON.stringify(unprotectedInfo)}
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<IndexPageServerSideProps> = async () => {
+  const resp = await fetch("http://localhost:3005/unprotected");
+  const respJson = await resp.json();
+
+  return { props: { unprotectedInfo: respJson } };
 };
 
 export default IndexPage;
